e00036425fbb5b6875674691877cf35ee08315f2
Genevieve <kj.genevieve@gmail.com>
2019-07-17 17:49:43 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,8 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  
+  adding_array = []
+  adding_array << "hey!"
 end
 
 def add_element_to_start_of_array(array, element)
