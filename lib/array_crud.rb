cebbde4bb60bea1fbca64ab71acbd06ec8c08581
Genevieve <kj.genevieve@gmail.com>
2019-07-17 17:50:10 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,7 +8,7 @@ end
 
 def add_element_to_end_of_array(array, element)
   adding_array = []
-  adding_array << "hey!"
+  adding_array << "arrays!"
 end
 
 def add_element_to_start_of_array(array, element)
