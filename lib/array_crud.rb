d2971a2c3a1227c457e974fb1a9ad77ae06d83b2
Genevieve <kj.genevieve@gmail.com>
2019-07-17 17:58:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -37,7 +37,7 @@ def retrieve_first_element_from_array(array)
 end
 
 def retrieve_last_element_from_array(array)
-  retrieve_last = ["wow", "arrays!", "am"]
+  retrieve_last = ["wow", "arrays!"]
   retrieve_last[-1]
 end
 
